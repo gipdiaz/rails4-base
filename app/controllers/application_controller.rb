b0e02a282a9b5f2294bd6d118fd4d52bae0e7a60
Gastón Díaz <gipdiaz@gmail.com>
2016-05-05 18:59:09 -0300

    changes view new session for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  I18n.locale = "es"
   protect_from_forgery with: :exception
   def after_sign_in_path_for(resource)
     current_user_path
